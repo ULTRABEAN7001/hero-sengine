75dd42028434ca4fc305f5885c5534e1039830f2
ULTRABEAN7001 <109013326+ULTRABEAN7001@users.noreply.github.com>
2024-10-14 23:00:11 +0800

    Minor visual additions
    Added gas trails and explanation

--- main.gd
+++ main.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var rotator = $Rotator
 @onready var fire = $Fire
 @onready var temperature = $Temperature
+@onready var explanation = $Explanation
 var temp: int
 
 func _process(delta):
@@ -14,6 +15,10 @@ func _process(delta):
 	elif rotator.C > 30:
 		fire.visible = true
 		fire.scale.x = rotator.C*0.01+0.25
-		
 
-	
+func _on_button_3_pressed():
+	if explanation.visible == false:
+		explanation.visible = true
+	else:
+		explanation.visible = false
+
--- rotator.gd
+++ rotator.gd
@@ -3,6 +3,12 @@ extends RigidBody2D
 var limit := 10000
 var C : int
 var is_water := true
+@onready var tail_1 = $tail1
+@onready var tail_2 = $tail2
+@onready var trail_timer = $trail_timer
+@onready var trail1 = $tail1/trail
+@onready var trail2 = $tail2/trail
+
 
 func _physics_process(delta):
 	print(constant_torque)
@@ -13,15 +19,25 @@ func _physics_process(delta):
 		
 	if C == 100 and is_water or C == -196 and is_water == false:
 		constant_torque = 35000
+		trail1.visible = true
+		trail2.visible = true
 	else: 
 		constant_torque = 0
+		trail1.visible = false
+		trail2.visible = false
 		
-
-
 
 func _on_button_pressed():
 	is_water = true
 
-
 func _on_button_2_pressed():
 	is_water = false
+	
+		
+
+
+
+
+
+	
+	
